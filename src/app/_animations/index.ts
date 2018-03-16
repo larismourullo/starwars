be0e3431ada6c9b7bec6cedd48c49652be4fb037
Larissa Mourullo <larismourullo@live.com>
2018-03-16 02:23:55 -0300

    :sparkles: Implementa Animação Entre Páginas

---
+++ src/app/_animations/index.ts
@@ -0,0 +1,2 @@
+export * from './fadein.animation';
+export * from './slideinout.animation';
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInAnimation } from '../_animations/index';
 
 @Component({
   selector: 'sw-categories',
+  animations: [fadeInAnimation],
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
+  host: { '[@fadeInAnimation]': '' }
 })
 export class CategoriesComponent implements OnInit {
 
