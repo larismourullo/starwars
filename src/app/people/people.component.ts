6106ff48b55187f3dc1b2a59faab583ffb98900b
Larissa Mourullo <larismourullo@live.com>
2018-03-16 02:23:03 -0300

    :art: Reorganiza pastas e arquivos do projeto

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../data.service';
+import { DataService } from '../services/data.service';
 import { Subscription } from 'rxjs/Subscription';
 
 @Component({
