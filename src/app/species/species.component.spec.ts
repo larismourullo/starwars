446aebc543bb8d3ed2ea0c18ddb184803e80bb95
Larissa Mourullo <larismourullo@live.com>
2018-03-17 10:03:25 -0300

    :sparkles: Implementa Componente Species

---
+++ src/app/species/species.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpeciesComponent } from './species.component';
+
+describe('SpeciesComponent', () => {
+  let component: SpeciesComponent;
+  let fixture: ComponentFixture<SpeciesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SpeciesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SpeciesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
