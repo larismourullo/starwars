2b53570c1a8790b834633a061314e0f6a1c47bcf
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 21:13:24 -0300

    :sparkles: Implementing new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ROUTING } from './app.routes'
 
 import { AppComponent } from './app.component';
-
+import { ButtonComponent } from './button/button.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    CategoriesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
