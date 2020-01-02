7707e57408eafc16a42a44c43c7fdd5e84879efe
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 21:19:06 -0300

    :art: Rearranging project files and folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ROUTING } from './app.routes';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './components/button/button.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { DataService } from './data.service';
+import { FooterComponent } from './components/footer/footer.component';
+import { DataService } from './services/data.service';
 import { PeopleComponent } from './people/people.component';
-import { LoadingComponent } from './loading/loading.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { LoadingComponent } from './loading/loading.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    ROUTING
+    ROUTING,
+    BrowserAnimationsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
