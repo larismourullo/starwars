eedb22f974e4ee5a3c0929050ab8aa8774d38e03
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 21:19:42 -0300

    :sparkles: Implementing name component

---
+++ src/app/components/name/name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sw-name',
+  templateUrl: './name.component.html',
+  styleUrls: ['./name.component.scss']
+})
+export class NameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
