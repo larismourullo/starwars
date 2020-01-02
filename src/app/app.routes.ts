743f4f8e5fceaf6d5c96d8e1553529e7878d34ed
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 21:13:55 -0300

    :sparkles: Creating a new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { CategoriesComponent } from './categories/categories.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PeopleComponent } from './people/people.component';
 
 export const AppRoutes: Routes = [
   { path: '', component: HeaderComponent },
-  { path: 'categories', component: CategoriesComponent }
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'people', component: PeopleComponent }
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
