72db86a15d2f4030d0f47d0233d12cab9633a97f
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 21:19:08 -0300

    :sparkles: Implementing animation when page change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInAnimation } from '../_animations/index';
 
 @Component({
   selector: 'sw-header',
+  animations: [fadeInAnimation],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  host: { '[@fadeInAnimation]': '' }
 })
 export class HeaderComponent implements OnInit {
 
