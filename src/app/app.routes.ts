c0b7d7ccc7b2f694fb26c90e4c14a9bba3e14c3a
Larissa Mourullo <larismourullo@gmail.com>
2020-01-01 21:14:38 -0300

    :sparkles: Implements new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,19 @@ import { CategoriesComponent } from './categories/categories.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PeopleComponent } from './people/people.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { SpeciesComponent } from './species/species.component';
+import { PlanetsComponent } from './planets/planets.component';
 
 export const AppRoutes: Routes = [
   { path: '', component: HeaderComponent },
   { path: 'categories', component: CategoriesComponent },
-  { path: 'people', component: PeopleComponent }
+  { path: 'people', component: PeopleComponent },
+  { path: 'starships', component: StarshipsComponent },
+  { path: 'vehicles', component: VehiclesComponent },
+  { path: 'species', component: SpeciesComponent },
+  { path: 'planets', component: PlanetsComponent },
 ];
 
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
